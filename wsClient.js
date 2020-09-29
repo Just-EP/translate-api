2e9bef3f06895c16b9a8e6797206220f20ea65f7
yangjiawei <jwyang@tsingsoft.com.cn>
2020-09-29 15:06:21 +0800

    websocket

---
+++ wsClient.js
@@ -0,0 +1,7 @@
+const WebSocket = require('ws');
+// 打开一个WebSocket:
+const ws = new WebSocket('ws://localhost:3000');
+// 响应onmessage事件:
+ws.onmessage = function(msg) { console.log(msg); };
+// 给服务器发送一个字符串:
+ws.send('Hello!');
---
+++ wsServer.js
@@ -0,0 +1,14 @@
+const ws = require('ws');
+const server = ws.Server;
+const wss = new server({port: 3000});
+wss.on('connection', function (ws) {
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    })
+});
